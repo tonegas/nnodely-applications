b592c2a4409a9fcf79bd392ac597723c29c6184f
tonegas <tonegas@gmail.com>
2026-02-25 15:05:56 +0100

    updated mass-spring-damper example

--- mass_spring_damper/mass_spring_damper.py
+++ mass_spring_damper/mass_spring_damper.py
@@ -14,32 +14,26 @@ from nnodely import *
 
 # Create neural model
 # List the input of the model
-x = Input('x') # Position of the mass
-x_target = Input('x_target') # Position of the mass
-dx = Input('dx') # Velocity of the mass
-F = Input('F') # Force
+x = Input('x')
+F = Input('F')
 
 # List the output of the model
-xk1 = Output('x[k+1]', Fir(W_init='init_negexp')(x.tw(0.2))+Fir(W_init='init_constant',W_init_params={'value':1})(F.last()))
-dxk1 = Output('dx[k+1]', Fir(Fir(W_init='init_negexp')(x.tw(0.2))+Fir(W_init='init_constant',W_init_params={'value':1})(F.last())))
+x_z_est = Output('x_z_est', Fir(x.tw(1)) + Fir(F.last()))
 
 # Add the neural models to the nnodely structure
 mass_spring_damper = Modely(seed=0)
-mass_spring_damper.addModel('xk1',xk1)
-mass_spring_damper.addModel('dxk1',dxk1)
+mass_spring_damper.addModel('x_z_est', x_z_est)
 
 # These functions are used to impose the minimization objectives.
 # Here it is minimized the error between the future position of x get from the dataset x.z(-1)
 # and the estimator designed useing the neural network. The miniminzation is imposed via MSE error.
-mass_spring_damper.addMinimize('next-pos', x_target.next(), xk1, 'mse')
-# The second minimization is between the velocity get from the dataset and the velocity estimator.
-mass_spring_damper.addMinimize('next-vel', dx.next(), dxk1, 'mse')
+mass_spring_damper.addMinimize('next-pos', x.z(-1), x_z_est, 'mse')
 
 # Nauralize the model and gatting the neural network. The sampling time depends on the datasets.
 mass_spring_damper.neuralizeModel(sample_time = 0.05) # The sampling time depends on the dataset
 
 # Data load
-data_struct = ['time',('x','x_target'),'dx','F']
+data_struct = ['time', 'x', 'dx', 'F']
 data_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)),'dataset','data')
 mass_spring_damper.loadData(name='mass_spring_dataset', source=data_folder, format=data_struct, delimiter=';')
 
@@ -47,19 +41,9 @@ mass_spring_damper.loadData(name='mass_spring_dataset', source=data_folder, form
 params = {'num_of_epochs': 50,
           'train_batch_size': 128,
           'lr':0.001}
-mass_spring_damper.trainModel(splits=[70,20,10], training_params = params)
+mass_spring_damper.trainAndAnalyze(splits=[70,20,10], training_params = params)
 
 # Add visualizer and show the results on the loaded dataset
 vis = MPLVisualizer()
 vis.setModely(mass_spring_damper)
-vis.showResult()
-
-## Recurrent training
-params = {'num_of_epochs': 10,
-          'train_batch_size': 128,
-          'lr':0.005}
-#Neural network train not reccurent training
-mass_spring_damper.trainModel(splits=[70,20,10], training_params = params, closed_loop={'x':'x[k+1]'}, prediction_samples=10,step=10)
-
-# Add visualizer and show the results on the loaded dataset
-vis.showResults()
+vis.showResult("mass_spring_dataset_val")
